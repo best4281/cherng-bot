9c0347ef9c2098750fcd4e5e1101fa94e63234f5
best4281 <thitisan2544@gmail.com>
2021-07-03 10:52:00 +0000

    Update message for version 0.0.2

--- cogs/update.py
+++ cogs/update.py
@@ -1,8 +1,8 @@
 from discord.ext import commands
 from configs import *
 
-update_message = "New command: `bonk` was added to this bot. For more info, try it out or use `"
-special_message = "Message from developer: *My midterm exam is also tomorrow, why am I doing this now...*\n*Please wish me luck*"
+update_message = "New command: `meme` is now available in this build of this bot. For more info, try it out or use `"
+special_message = ""
 
 def get_prefix_from_guild(guild):
     with open(prefixFile, 'r') as f:
@@ -18,13 +18,10 @@ class UpdateCog(commands.Cog, command_attrs = {"hidden" : True}):
         self.bot = bot
 
     @commands.command(name = "sendupdate", aliases = [])
+    @commands.is_owner()
     async def send_update(self, ctx):
-        if ctx.author.id != 283765324401344514:
-            return
         for guild in self.bot.guilds:
-            await guild.system_channel.send(f"{update_message}{get_prefix_from_guild(guild)}help bonk`\n{special_message}")
-            #if guild.id == 359593138408521728:
-            #    await guild.system_channel.send(special_message)
+            await guild.system_channel.send(f"{update_message}{get_prefix_from_guild(guild)}help meme`\n{special_message}")
 
 def setup(bot):
     bot.add_cog(UpdateCog(bot))
